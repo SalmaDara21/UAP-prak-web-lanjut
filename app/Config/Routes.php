cceae563ce50cd680f1c3db78ce8607114d141a8
MRadityaAdhirajasa <radityaadhirajasaa@gmail.com>
2023-11-07 09:01:35 +0700

    profile read dan update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,4 +20,10 @@ $routes->get('/menu', 'Home::menu');
 // $routes->get('/test', [Home::class, 'test']);
 $routes->get('/admin', 'Admin::index',['filter'=> 'role:admin']);
 $routes->get('/admin/index', 'Admin::index',['filter'=> 'role:admin']);
+$routes->get('/user_list', 'Admin::user_list');
+$routes->get('/admin/(:num)', 'Admin::detail/$1');
+$routes->get('/profile', 'User::profile');
+$routes->get('/profile/edit', 'User::edit');
+$routes->post('/profile/update', 'User::update');
+
 
--- app/Controllers/admin.php
+++ app/Controllers/admin.php
@@ -2,13 +2,60 @@
 
 namespace App\Controllers;
 
+
+
+
 class Admin extends BaseController
 {
     public function index(): string
     {
+
         $data = [
-            'title' => 'Admin'
+            'title' => 'Admin',
         ];
+
         return view('Dashboard_admin',$data);
+    }
+
+    public function user_list()
+    {
+
+        $data['title'] = 'User_List';
+
+        // $users = new \myth\auth\Models\UserModel();
+        // $data['users'] = $users->findAll();
+
+        $db = \config\Database::connect();
+        $builder = $db->table('users');
+        $builder->select('users.id as userid, username, email, name');
+        $builder->join('auth_groups_users', 'auth_groups_users.user_id = users.id');
+        $builder->join('auth_groups', 'auth_groups.id = auth_groups_users.group_id');
+        $query = $builder->get();
+
+        $data['users'] = $query->getResult();
+
+        return view('user_list',$data);
+    }
+
+    public function detail($id = 0)
+    {
+
+        $data['title'] = 'detail';
+
+        $db = \config\Database::connect();
+        $builder = $db->table('users');
+        $builder->select('users.id as userid, username, email, fullname, user_image, name');
+        $builder->join('auth_groups_users', 'auth_groups_users.user_id = users.id');
+        $builder->join('auth_groups', 'auth_groups.id = auth_groups_users.group_id');
+        $builder->where('users.id', $id);
+        $query = $builder->get();
+
+        $data['user'] = $query->getRow();
+
+        if(empty($data['user'])) {
+            return redirect()->to('/user_list');
+        }
+
+        return view('detail',$data);
     }
 }
